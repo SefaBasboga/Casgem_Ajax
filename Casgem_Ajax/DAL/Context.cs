b4d89a4fdf291b9388526dd2d30ba7661a260cc0
Sefa Başboğa <sefabsbgaaa@gmail.com>
2023-08-01 01:14:32 +0300

    Add project files.

---
+++ Casgem_Ajax/DAL/Context.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Casgem_Ajax.DAL
+{
+    public class Context :DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-H5NLS4J;Initial Catalog=CasgemAjaxDb; integrated Security=true;TrustServerCertificate=true;");
+  
+        }
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
